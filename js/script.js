2f2d4bb50a3da3b81c98578d2a5cecff0ebddb8e
unknown <joelmenacho@gmail.com>
2022-09-05 17:19:26 -0500

    7cambio home

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const profile_img = document.getElementById('perfil_img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
 
-setTimeout(getData, 750)
+setTimeout(getData, 1250)
 
 function getData() {
   header.innerHTML =
-    '<img src="https://i.postimg.cc/WpSsB4rd/934-1.jpg" width="160" />'
-  title.innerHTML = 'Comsitec System'
-  excerpt.innerHTML = 'Nuestro primer proyecto en desarollo web'
+    '<img src="https://assets.materialup.com/uploads/bb860163-d0a3-4755-9088-798e19bd6609/preview.png" width="160" />'
+  title.innerHTML = 'Comsitec Store'
+  excerpt.innerHTML = 'Con el objetivo de incrementar las ventas, desarrollamos una página web E-commerce para la marca Comsitec Store'
   profile_img.innerHTML =
     '<img src="https://joelmenacho.github.io/HogarMenacho/img/HM_CABECERA.png" alt="" />'
   name.innerHTML = 'Joel Menacho'
